28cbba99d58a0965a920a2af87ec767800ac6a07
Arvin <yuanfensky11@163.com>
2017-08-24 23:42:07 +0800

    Create main.py

---
+++ device/src/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#This is the file executing while STM32 MCU bootup, and in this file,  
+it will call other functions to fullfill the project.
+
+if __name__="__main__":
+  print('Smart IoT Plant System')
