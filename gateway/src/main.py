a19aff3173a85ca8b0035873c3a6315d3dbedba4
Arvin <yuanfensky11@163.com>
2017-08-28 23:20:28 +0800

    Create main.py

---
+++ gateway/src/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#Gateway main script.
+#Communicate with end devices via LoRa.
+#Communicate with server via MQTT(hbmqtt) and HTTP POST.
+#Save data in the sqlite database.
+#Parse JSON from MQTT and LoRa protocol.
