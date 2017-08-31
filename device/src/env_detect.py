031544fef1bad7f0b08396134f251ad17d3aa5b8
Arvin <yuanfensky11@163.com>
2017-08-31 15:09:01 +0800

    Update env_detect.py

--- device/src/env_detect.py
+++ device/src/env_detect.py
@@ -7,4 +7,11 @@
 #Compile the DHT in firmware, then use DHT lib in application.
 #Raining, same to soil moisture.
 #Raining ? DO value: 0 
+from pyb import Pin
+p_in = Pin('Y12', Pin.IN, Pin.PULL_UP)
+p_in.value
 
+
+adc = pyb.ADC(Pin('Y11'))       # create an analog object from a pin
+adc = pyb.ADC(pyb.Pin.board.Y11)
+val = adc.read()                # read an analog value
