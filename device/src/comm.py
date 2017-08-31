9e77d2507755cdab9fc58699afb20dca3a1879ff
Arvin <yuanfensky11@163.com>
2017-08-31 15:04:54 +0800

    Update comm.py

--- device/src/comm.py
+++ device/src/comm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#Communication module: LoRa.
 #Communication method with gateway via LoRa.
 #Uart port drive LoRa module.
 #Parse JSON between device and gateway via LoRa channel.
