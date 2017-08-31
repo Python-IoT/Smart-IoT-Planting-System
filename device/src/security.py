79dea4623c500e7a19abc2456ac4ed9ba2f85dc4
Arvin <yuanfensky11@163.com>
2017-08-31 14:22:38 +0800

    Update security.py

--- device/src/security.py
+++ device/src/security.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 #receive signal from infrared sensor
+#Sensor: HC-SR501
+#VCC, Signal <--> GPIO Y12, GND
+from pyb import Pin
+motion_detect = Pin('Y12',Pin.IN,Pin,PULL_UP)
+if(motion_detect.value() == 1):
+  print('alarm')
