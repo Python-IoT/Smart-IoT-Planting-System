ef99371ff77d4ead4e8edc6049a9b3f7f18e2f85
Arvin <yuanfensky11@163.com>
2017-08-31 14:51:31 +0800

    Update env_detect.py

--- device/src/env_detect.py
+++ device/src/env_detect.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 #detect environment information from several sensors:
-water leverl, humity, oil water, temperature, light sensitivity
-rain strength.
+#water leverl, humity, oil water, temperature, light sensitivity
+#rain strength.
+#Air temperature&humity sensor: DHT11.
+#Add dht.py in micropython/stmhal/modules， refer to esp8266
+#Compile the DHT in firmware, then use DHT lib in application.
