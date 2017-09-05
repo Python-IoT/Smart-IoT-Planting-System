98147114cec73da6cc660055455b5ec07906e156
Arvin <yuanfensky11@163.com>
2017-09-05 14:31:24 +0800

    Update dev_mag.py

--- device/src/dev_mag.py
+++ device/src/dev_mag.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 #Features as below:
 #1. Detect the battery's power left via ADC port.
-#2. Check the device on-line or off-line status by hbmqtt heartbeat.  
+#2. Check the device on-line or off-line status by LoRa heartbeat.  
 
