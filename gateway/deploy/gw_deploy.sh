a56fb19c9518b3199a8a5e89e43d73842b99fcfb
Arvin <yuanfensky11@163.com>
2017-09-08 15:25:57 +0800

    Create gw_deploy.sh

---
+++ gateway/deploy/gw_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Script for deploy & preparation for gateway software.
+#install requests lib
+pipenv install requests
+
+#install pip3
+#install virtualenv
+#install hbmqtt
+pip install hbmqtt
