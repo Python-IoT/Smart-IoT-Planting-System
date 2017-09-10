cd1adccaa47be31f6d29f4122c08c880d4730fdc
Arvin <yuanfensky11@163.com>
2017-09-10 22:33:23 +0800

    Update post.py

--- gateway/src/post.py
+++ gateway/src/post.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
 import requests
 HTTP POST message to cloud platform
+#Most times gateway communicate with cloud via MQTT(hbmqtt framework),  
+#in some special time, it need to use HTTP(requests framework) to POST message to cloud.
