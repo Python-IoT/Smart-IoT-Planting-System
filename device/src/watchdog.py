43adddce6976dbb45d4cf474f820193584b2ea55
Arvin <yuanfensky11@163.com>
2017-09-15 16:58:59 +0800

    Create watchdog.py

---
+++ device/src/watchdog.py
@@ -0,0 +1,3 @@
+#Detect device status, if errors, reboot by watchdog.
+#Send on-line message to gateway while bootup, if gateway receive plenty of 
+#on-line messages in a short time from the same device, an alarm occurs.
