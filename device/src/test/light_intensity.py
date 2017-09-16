1f02133b6a754b479d112d1d76e6e8cfb9a3670e
Arvin <yuanfensky11@163.com>
2017-09-16 22:02:02 +0800

    Update light_intensity.py

--- device/src/test/light_intensity.py
+++ device/src/test/light_intensity.py
@@ -4,7 +4,8 @@ import py
 #Import light intensity needed module 
 import LightIntensity
 import time
- 
+
+print('light intensity module test')
 if __name__=='__main__':
   while True:
     print(LightIntensity.readLight())
