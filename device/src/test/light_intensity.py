e074cdf62f6e70646636783f71f31a39bd21d87a
Arvin <yuanfensky11@163.com>
2017-09-16 22:03:13 +0800

    Update light_intensity.py

--- device/src/test/light_intensity.py
+++ device/src/test/light_intensity.py
@@ -1,5 +1,5 @@
 #This scripyt is used to test module LightIntensity.
-#Obtain light intensity data from sensor GY-30.
+#Obtain light intensity data from sensor GY-30 and display it.
 import py
 
 #Import light intensity needed module 
