6b971074212249e3fff7d43f9ef0e6fd7796678c
Arvin <yuanfensky11@163.com>
2017-09-18 15:53:13 +0800

    Update LoRa_test.py

--- gateway/src/test/LoRa_test.py
+++ gateway/src/test/LoRa_test.py
@@ -1,1 +1,37 @@
 # LoRa_test.py
+#Communication module: LoRa.
+#Communication method with device via LoRa.
+#Uart port drive LoRa module.
+#Parse JSON between device and gateway via LoRa channel.
+#LoRa module: E32-TTL-100
+#Pin specification:
+#M0    <--> GPIO(OUT)     #mode setting
+#M1    <--> GPIO(OUT)     #mode setting
+#RXD   <--> 8(TXD)        #ttyS0
+#TXD   <--> 10(RXD)       #ttyS0
+#AUX   <--> GPIO/INT(IN)  #module status detecting
+#VCC
+#GND
+
+#Install pyserial:
+#pip install pyserial
+
+#!/usr/bin/env python
+import serial  
+import time  
+pyserial_test = serial.Serial("/dev/ttyS0", 115200)  
+def main():  
+    while True:  
+        count = pyserial_test.inWaiting()  
+        if count != 0:  
+            recv = "pi return: "+pyserial_test.read(count)+"\n"  
+            pyserial_test.write(recv)  
+        pyserial_test.flushInput()  
+        time.sleep(0.1)  
+  
+if __name__ == '__main__':  
+    try:  
+        main()  
+    except KeyboardInterrupt:  
+        if pyserial_test != None:  
+            pyserial_test.close()  
