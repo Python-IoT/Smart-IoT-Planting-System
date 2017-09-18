d17989243fe6d347fb93c105ece998384d7d64c4
Arvin <yuanfensky11@163.com>
2017-09-18 15:28:32 +0800

    Update and rename comm.py to LoRa.py

---
+++ device/src/LoRa.py
@@ -0,0 +1,15 @@
+#    LoRa.py
+#Communication module: LoRa.
+#Communication method with gateway via LoRa.
+#Uart port drive LoRa module.
+#Parse JSON between device and gateway via LoRa channel.
+#LoRa module: E32-TTL-100
+#Pin specification:
+#M0    <--> GPIO(OUT)     #mode setting
+#M1    <--> GPIO(OUT)     #mode setting
+#RXD   <--> X1(TX)        #UART4
+#TXD   <--> X2(RX)        #UART4
+#AUX   <--> GPIO/INT(IN)  #module status detecting
+#VCC
+#GND
+
--- device/src/comm.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#Communication module: LoRa.
-#Communication method with gateway via LoRa.
-#Uart port drive LoRa module.
-#Parse JSON between device and gateway via LoRa channel.
