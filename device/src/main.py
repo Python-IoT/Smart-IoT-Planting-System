c000cad21454553ab3e33194a12d2527474174e0
Arvin <yuanfensky11@163.com>
2017-10-10 14:32:13 +0800

    Update main.py

--- device/src/main.py
+++ device/src/main.py
@@ -1,8 +1,25 @@
 #This is the file executing while STM32 MCU bootup, and in this file,  
 #it will call other functions to fullfill the project.
+#Communication module: LoRa.
+#Communication method with gateway via LoRa.
+#Uart port drive LoRa module.
+#Parse JSON between device and gateway via LoRa channel.
+#LoRa module: E32-TTL-100
+#Pin specification:
+#Module         MCU
+#M0(IN)    <--> GPIO(X3)(OUT)     #mode setting, can not hang
+#M1(IN)    <--> GPIO(X4)(OUT)     #mode setting, can not hang
+#RXD(IN)   <--> X1(TX)(OUT)   #UART4
+#TXD(OUT)  <--> X2(RX)(IN)    #UART4
+#AUX(OUT)  <--> GPIO/INT(IN)  #module status detecting
+#VCC
+#GND
+#Communication mode is 0, need to set M0 and M1 to 0.
+
 import pyb
 from pyb import Pin
 from pyb import Timer
+from pyb import UART  
 import micropython
 
 #Import light intensity needed module 
@@ -15,59 +32,28 @@ print('pin init')
 Pin('Y11',Pin.OUT_PP).low() #GND
 Pin('Y9',Pin.OUT_PP).high() #VCC
 
+#Set LoRa module with mode-0.
+M0 = Pin('X3', Pin.OUT_PP)
+M1 = Pin('X4', Pin.OUT_PP)
+M0.low()
+M1.low()
+#Init uart4 for LoRa module.
+u4 = UART(4,9600)  
+u4.init(9600, bits=8, parity=None, stop=1)  
+cmd_online = '{"ID":1, "CMD":Online, "TYPE":"N", "VALUE":"N"}'
+u4.write(cmd_online)
+
+
+
 #LED shining regularly(using timer) to indicate the program is running correctly
 tim1 = Timer(1, freq=1)
 tim1.callback(lambda t: pyb.LED(1).toggle())
  
 if __name__=='__main__':
   while True:
-    print('Smart IoT Plant System-Device')
-    print(LightIntensity.readLight())
-    time.sleep(2)
+    #Waiting for the message from UART4 to obtain LoRa data.
+    len = u4.any()
+    if(len > 0): 
+      print(u4.read())
 
  
- #send on-line message to gateway to notifiy and obtain own data from gateway's database
-
-  
-###reference begin###
-"""
-import pyb
-from pyb import Pin
-from ds18x20 import DS18X20
-from pyb import Timer
-import micropython
-
-micropython.alloc_emergency_exception_buf(100)
-tempValue = 0
-print('pin init')
-Pin('Y11',Pin.OUT_PP).low() #GND
-Pin('Y9',Pin.OUT_PP).high() #VCC
-
-def displayTemp(t):
- print('Current Temperature:')
- print(tempValue)
- 
-tim1 = Timer(1)
-tim1.callback(displayTemp)
-tim1.init(freq=1/5)
- 
-if __name__=='__main__':
- print('Smart IoT Plant System')
- DQ=DS18X20(Pin('Y10')) #DQ
- while True:
-  tempValue = DQ.read_temp()
- ###reference end###
- """
-"""
-Waiting for LoRa message from gateway.
-from pyb import UART
-u4 = UART(4,9600)  
-u4.init(9600, bits=8, parity=None, stop=1)  
-while True:
-    if(u4.any() > 0):
-        print('reading....')
-        receive = u4.read()
-        print(receive)
-        u4.write(receive)	
-  
-"""
