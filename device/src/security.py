35cc8a0ad3a4c3addb8b0dec391abd8f2eccdcd4
Arvin <yuanfensky11@163.com>
2017-10-13 16:56:09 +0800

    Update security.py

--- device/src/security.py
+++ device/src/security.py
@@ -6,9 +6,9 @@
 #GND
 import pyb
 from pyb import Pin
-motion_detect = Pin('X11',Pin.IN,Pin,PULL_UP)
+motion_detect = Pin('X11',Pin.IN,Pin.PULL_UP)
 #if motion sensor value is 1, otherwise 0.
 def detectMotion():
   print('in security module')
-  returen motion_detect.value()
+  return motion_detect.value()
 
