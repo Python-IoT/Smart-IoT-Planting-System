baeff434000ac5cc6513907f08f86ce9b5e16098
Arvin <yuanfensky11@163.com>
2017-10-13 16:52:18 +0800

    Update security.py

--- device/src/security.py
+++ device/src/security.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 #receive signal from infrared sensor
 #Sensor: HC-SR501
-#VCC, Signal <--> GPIO Y12, GND
+#VCC 
+#Signal <--> GPIO(IN) X11
+#GND
+import pyb
 from pyb import Pin
-motion_detect = Pin('Y12',Pin.IN,Pin,PULL_UP)
-if(motion_detect.value() == 1):
-  print('alarm')
+motion_detect = Pin('X11',Pin.IN,Pin,PULL_UP)
+#if motion sensor value is 1, otherwise 0.
+def detectMotion():
+  print('in security module')
+  returen motion_detect.value()
+
