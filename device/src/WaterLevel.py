9873891a9f26edc51a22e51b5910615a7e08d410
Arvin <yuanfensky11@163.com>
2017-10-17 15:21:15 +0800

    Create WaterLevel.py

---
+++ device/src/WaterLevel.py
@@ -0,0 +1,15 @@
+#Water level sensor.
+#VCC 
+#GND
+#AO <--> ADC Port(A7) Analog data
+
+#AO is the specific value.
+
+import pyb
+
+adc = pyb.ADC(Pin('A7'))       # create an analog object from a pin
+adc = pyb.ADC(pyb.Pin.board.A7)
+# read an analog value
+def getWaterLevel():               
+  print('WaterLevel Ao')
+  return adc.read()
