302d9f22dc00661b98c113eb089e65c219ca74a2
Arvin <yuanfensky11@163.com>
2017-10-17 15:05:06 +0800

    Update moisture.py

--- device/src/moisture.py
+++ device/src/moisture.py
@@ -6,6 +6,10 @@
 #if value is low than defined data, DO value is 0, 
 #if value is high than defined data, DO value is 1.
 #AO is the specific value.
+#Sensor value reference:
+#0 ~300 : dry soil
+#300~700 : wet soil 
+#700~950 : in water
 import pyb
 from pyb import Pin
 p_in = Pin('Y12', Pin.IN, Pin.PULL_UP)
