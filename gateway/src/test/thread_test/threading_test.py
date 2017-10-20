600239128cd131846c7998a9eb9605aee56736f6
Arvin <yuanfensky11@163.com>
2017-10-20 15:53:53 +0800

    Update threading_test.py

--- gateway/src/test/thread_test/threading_test.py
+++ gateway/src/test/thread_test/threading_test.py
@@ -4,18 +4,18 @@ from time import ctime,sleep
 
 def Lora(func):
   while True:
-    print("This is %s. %d" % (func,ctime()))  
+    print("This is %s. %s" % (func,ctime()))
     sleep(1)
 
 def Lora_json(func):
   while True:
-    print("This is %s. %d" % (func,ctime()))  
+    print("This is %s. %s" % (func,ctime()))
     sleep(1)
 
 threads = []
-t1 = threading.Thread(target=Lora,args=('Lora Thread'))
+t1 = threading.Thread(target=Lora,args=('Lora Thread',))
 threads.append(t1)
-t2 = threading.Thread(target=Lora_json,args=('Lora_json_parse Thread'))
+t2 = threading.Thread(target=Lora_json,args=('Lora_json_parse Thread',))
 threads.append(t2)
 
 if __name__ == '__main__':
@@ -25,4 +25,3 @@ if __name__ == '__main__':
   while True:
    print("\nThis is the main thread!")
    sleep(2)
-
