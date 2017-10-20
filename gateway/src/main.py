460b576cbddbbb69ac641f3faa1a7b8fc9a9a56c
Arvin <yuanfensky11@163.com>
2017-10-20 16:23:15 +0800

    Update main.py

--- gateway/src/main.py
+++ gateway/src/main.py
@@ -52,16 +52,16 @@ def Lora_json(func):
       if int(json_lora.get("ID")) == 1 : #Device ID-1 existed in gateway database
         if json_lora.get("CMD") == 'Online':
           response = '{"ID":"1", "CMD":"Online", "TYPE":"Light2", "VALUE":"On"}'
-            print(response)
-            ser.write(response)
-          elif json_lora.get("CMD") == 'Env':
-            if json_lora.get("TYPE") == 'moisture':
-              if int(json_lora.get("VALUE")) < 2000: # soil moisture is lower than standard
-                response = '{"ID":"1", "CMD":"irrigate", "TYPE":"Open", "VALUE":"100"}'
-                ser.write(response)
-        else:
-          print('init_device')
-          #init_device()  #Create sqlite table for device 1.
+          print(response)
+          ser.write(response)
+        elif json_lora.get("CMD") == 'Env':
+          if json_lora.get("TYPE") == 'moisture':
+            if int(json_lora.get("VALUE")) < 2000: # soil moisture is lower than standard
+              response = '{"ID":"1", "CMD":"irrigate", "TYPE":"Open", "VALUE":"100"}'
+              ser.write(response)
+      else:
+        print('init_device')
+        #init_device()  #Create sqlite table for device 1.
       
     print("This is %s. %s" % (func,ctime()))
     sleep(1)
