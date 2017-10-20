218bad5ce6b9cce9eeef4ae2150555b4b4a96171
Arvin <yuanfensky11@163.com>
2017-10-20 15:46:40 +0800

    Update threading_test.py

--- gateway/src/test/thread_test/threading_test.py
+++ gateway/src/test/thread_test/threading_test.py
@@ -13,15 +13,15 @@ def Lora_json(func):
     sleep(1)
 
 threads = []
-t1 = threading.Thread(target=music,args=('Lora Thread'))
+t1 = threading.Thread(target=Lora,args=('Lora Thread'))
 threads.append(t1)
-t2 = threading.Thread(target=move,args=('Lora_json_parse Thread'))
+t2 = threading.Thread(target=Lora_json,args=('Lora_json_parse Thread'))
 threads.append(t2)
 
 if __name__ == '__main__':
   for t in threads:
 #        t.setDaemon(True)
-  t.start()
+    t.start()
   while True:
    print("\nThis is the main thread!")
    sleep(2)
