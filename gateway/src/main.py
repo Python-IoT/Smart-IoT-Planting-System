4cd1c14ccc298aed0a1d68a82b7992a94832572b
Arvin <yuanfensky11@163.com>
2017-10-20 16:01:26 +0800

    Update main.py

--- gateway/src/main.py
+++ gateway/src/main.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
+#Communicate with end devices via LoRa.
+#Communicate with server via MQTT(hbmqtt) and HTTP POST.
+#Save data in the sqlite database.
+#Parse JSON from MQTT and LoRa protocol.
+
+#Communication module: LoRa.
+#Communication method with device via LoRa.
+#Uart port drive LoRa module.
+#Parse JSON between device and gateway via LoRa channel.
+#LoRa module: E32-TTL-100
+#Pin specification:
+#M0    <--> GPIO(OUT)     #mode setting  connct to GND is OK!(Low)
+#M1    <--> GPIO(OUT)     #mode setting  connct to GND is OK!(Low)
+#RXD   <--> 8(TXD)        #ttyS0
+#TXD   <--> 10(RXD)       #ttyS0
+#AUX   <--> GPIO/INT(IN)  #module status detecting
+#VCC
+#GND
+
+#You need to install pyserial manually, install command is below:
+#pip install pyserial
+
+import serial
+import time
+import json
+#ser  = serial.Serial("/dev/ttyS0", 9600)
+ser  = serial.Serial("/dev/ttyS0", 9600, timeout=0.2)
 import threading
 from time import ctime,sleep
 
