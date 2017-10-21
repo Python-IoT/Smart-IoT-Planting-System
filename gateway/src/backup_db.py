3dd83502433bd25f79c37909761355122bd62232
Arvin <yuanfensky11@163.com>
2017-10-21 22:01:04 +0800

    Update backup_db.py

--- gateway/src/backup_db.py
+++ gateway/src/backup_db.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
+#0:0:07 everyday upload gateway.db to server to backup
 import time
 from datetime import datetime, timedelta
 from time import sleep
 
 SECONDS_PER_DAY = 24 * 60 * 60
 
-
 def doFunc():
-    print "do Function..."
-
+    print('do Function...')
 
 def doFirst():
-    curTime = datetime.now()
-    print curTime
-    desTime = curTime.replace(hour=20, minute=0, second=0, microsecond=0)
-    print desTime
-    delta = desTime-curTime
-    sleeptime = delta.total_seconds()
-    print "Now day must sleep %d seconds" %     sleeptime
-    sleep(sleeptime)
-    doFunc()
-
+  curTime = datetime.now()
+  print(curTime)
+  desTime = curTime.replace(hour=0, minute=0, second=7, microsecond=0) #0:0:07
+  print(desTime)
+  delta = desTime-curTime
+  sleeptime = delta.total_seconds()
+  print("Now day must sleep %d seconds" % sleeptime)
+  sleep(sleeptime)
+  doFunc()
 
 if __name__ == "__main__":
-    doFirst()
+  doFirst()
