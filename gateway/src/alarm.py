68559cc4c7d9f32ed30e0f818a6b3c94349c8fc5
Arvin <yuanfensky11@163.com>
2017-12-08 17:13:28 +0800

    Update alarm.py

--- gateway/src/alarm.py
+++ gateway/src/alarm.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 #Call a specific phone number or send SMS via GA6 GSM module 
+#Hardware connection method:
+#Gateway(Pi)       GA6
+#5V      <-->    5V
+#GND     <-->    GND
+#(#8)TX  <-->    URX
+#(#10)RX <-->    UTX
+
