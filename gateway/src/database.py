93f50fd446f16e486b2b01744a152d186e161f1e
Arvin <yuanfensky11@163.com>
2017-12-13 23:17:03 +0800

    Update database.py

--- gateway/src/database.py
+++ gateway/src/database.py
@@ -1,2 +1,7 @@
+#!/usr/bin/env python
+import sqlite3
 #Using sqlite to store the data of devices.
 #os.system('./db_init.sql') #create database and init the tables
+def create_db():
+  con = sqlite3.connect('gw.db')
+  print(con)
