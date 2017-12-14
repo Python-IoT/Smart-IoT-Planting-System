6bf0e7347e875fca8cc19c4c26e34bbb84765e20
Arvin <yuanfensky11@163.com>
2017-12-14 22:33:03 +0800

    Update database.py

--- cloud/database.py
+++ cloud/database.py
@@ -22,3 +22,17 @@ def create_table():
 
   cursor.execute(sql)
   db.close()
+  
+def db_insert():
+  db = pymysql.connect("localhost","sips","root","zaijian" )
+  cursor = db.cursor()
+  sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+
+  try:
+    cursor.execute(sql)
+    db.commit()
+  except:
+    db.rollback()
+  db.close()
