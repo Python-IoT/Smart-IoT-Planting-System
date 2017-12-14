64b2855c1f2066e868570d5e65c945c935c4e7e0
Arvin <yuanfensky11@163.com>
2017-12-14 22:31:22 +0800

    Update database.py

--- cloud/database.py
+++ cloud/database.py
@@ -8,3 +8,17 @@ data = cursor.fetchone()
  
 print ("Database version : %s " % data)
 db.close()
+
+def create_table():
+ db = pymysql.connect("localhost","sips","root","zaijian" )
+  cursor = db.cursor()
+  cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+  sql = """CREATE TABLE EMPLOYEE (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,  
+         SEX CHAR(1),
+         INCOME FLOAT )"""
+
+  cursor.execute(sql)
+  db.close()
